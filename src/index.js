4e79e1eb0cd7468dbf2582e3ff8409f744014069
jmeade11 <jennifer.meade@general.assemb.ly>
2019-07-15 10:04:42 -0400

    Clean up scss files and Close #19
    Clean up all of the scss files and Close #19 by removing the unused
    styles for the flash messages.  Also removed the App.scss (can be added
    by devs if they need it). Added an index.scss so that the Bootstrap sass
    file is being brought in there to allow for Bootstrap sass variable
    overrides.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './css/index.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.scss'
+
 import App from './App'
 import { HashRouter } from 'react-router-dom'
 
