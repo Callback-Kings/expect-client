33fa563e5a17f98e3ee9bfcd96e6d9b96e1c9358
jmeade11 <jennifer.meade@general.assemb.ly>
2019-07-15 11:11:38 -0400

    Use React-Bootstrap Navbar component
    Replaced the custom header to just use the React-Bootstrap Navbar
    component.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,41 @@
 import React, { Fragment } from 'react'
-import { Link } from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 const authenticatedOptions = (
   <Fragment>
-    <Link to="/change-password">Change Password</Link>
-    <Link to="/sign-out">Sign Out</Link>
+    <Nav.Link href="#change-password">Change Password</Nav.Link>
+    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
 )
 
 const unauthenticatedOptions = (
   <Fragment>
-    <Link to="/sign-up">Sign Up</Link>
-    <Link to="/sign-in">Sign In</Link>
+    <Nav.Link href="#sign-up">Sign Up</Nav.Link>
+    <Nav.Link href="#sign-in">Sign In</Nav.Link>
   </Fragment>
 )
 
 const alwaysOptions = (
   <Fragment>
-    <Link to="/">Home</Link>
+    <Nav.Link to="/">Home</Nav.Link>
   </Fragment>
 )
 
 const Header = ({ user }) => (
-  <header className="main-header">
-    <h1>Uber, But For Taxis</h1>
-    <nav>
-      { user && <span>Welcome, {user.email}</span>}
-      { user ? authenticatedOptions : unauthenticatedOptions }
-      { alwaysOptions }
-    </nav>
-  </header>
+  <Navbar bg="primary" variant="dark" expand="md">
+    <Navbar.Brand href="#">
+      react-auth-template
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+        { alwaysOptions }
+        { user ? authenticatedOptions : unauthenticatedOptions }
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 export default Header
