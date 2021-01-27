2d3267298f79ce1d90a012ea6185dc7341b83c72
Shane James <shanejames@Shanes-iMac.local>
2021-01-26 22:23:47 -0500

    Add tours data add show tours component showing six tours for sale add show tour, react front-end routing add buttons to tours

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://safe-dawn-20664.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
---
+++ src/data/tourData.js
@@ -0,0 +1,46 @@
+const tours = [
+  {
+    id: 1,
+    location: 'Miami',
+    date: '2021-06-01',
+    price: '50',
+    image: 'https://img.pngio.com/miami-beach-in-miamis-south-beach-neighborhood-malendyer-miami-beach-png-977_550.png'
+  },
+  {
+    id: 2,
+    location: 'Denver',
+    date: '2021-02-01',
+    price: '90',
+    image: 'https://wallpapercave.com/wp/wp2521459.jpg'
+  },
+  {
+    id: 3,
+    location: 'Los Angeles',
+    date: '2021-03-10',
+    price: '100',
+    image: 'https://cdn.vox-cdn.com/thumbor/woz7U4ue4EvMJIk3YQKjsWt_qKU=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/11737883/hollywood_street_shutterstock.jpg'
+  },
+  {
+    id: 4,
+    location: 'Manhattan',
+    date: '2021-11-01',
+    price: '120',
+    image: 'https://i.pinimg.com/originals/41/dd/c5/41ddc5db82c7ee847ce64a3ccfe04f2f.jpg'
+  },
+  {
+    id: 5,
+    location: 'Chicago',
+    date: '2021-10-01',
+    price: '75',
+    image: 'https://ulidigitalmarketing.blob.core.windows.net/ulidcnc/2019/03/ULIHomepage-1200x675.jpg'
+  },
+  {
+    id: 6,
+    location: 'Dallas',
+    date: '2021-04-01',
+    price: '75',
+    image: 'https://www.mymove.com/wp-content/uploads/2020/03/Dallas-skyline.jpg'
+  }
+]
+
+export default tours
