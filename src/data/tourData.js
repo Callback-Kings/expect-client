fca7d00e3eab4add922b7f82a4aedf8c0287e394
senoralikz <alexjm825@gmail.com>
2021-01-29 16:21:36 -0500

    Added more tour destinations in tourData

--- src/data/tourData.js
+++ src/data/tourData.js
@@ -4,42 +4,70 @@ const purchases = [
     location: 'Miami',
     date: '2021-06-01',
     price: '50',
-    image: 'https://img.pngio.com/miami-beach-in-miamis-south-beach-neighborhood-malendyer-miami-beach-png-977_550.png'
+    image: 'https://wallpapercave.com/wp/wp2603552.jpg'
   },
   {
     id: 2,
     location: 'Denver',
     date: '2021-02-01',
     price: '90',
-    image: 'https://wallpapercave.com/wp/wp2521459.jpg'
+    image: 'https://denvergov.org/files/assets/public/global-ccd/images/denver-sunset-bg.jpg'
   },
   {
     id: 3,
     location: 'Los Angeles',
     date: '2021-03-10',
     price: '100',
-    image: 'https://cdn.vox-cdn.com/thumbor/woz7U4ue4EvMJIk3YQKjsWt_qKU=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/11737883/hollywood_street_shutterstock.jpg'
+    image: 'https://i.redd.it/qbrdv8m8yog41.jpg'
   },
   {
     id: 4,
     location: 'Manhattan',
     date: '2021-11-01',
     price: '120',
-    image: 'https://i.pinimg.com/originals/41/dd/c5/41ddc5db82c7ee847ce64a3ccfe04f2f.jpg'
+    image: 'https://i.imgur.com/6TH1n38.jpg'
   },
   {
     id: 5,
     location: 'Chicago',
     date: '2021-10-01',
     price: '75',
-    image: 'https://ulidigitalmarketing.blob.core.windows.net/ulidcnc/2019/03/ULIHomepage-1200x675.jpg'
+    image: 'https://wallpaperaccess.com/full/1137364.jpg'
   },
   {
     id: 6,
     location: 'Dallas',
     date: '2021-04-01',
     price: '75',
-    image: 'https://www.mymove.com/wp-content/uploads/2020/03/Dallas-skyline.jpg'
+    image: 'https://wallpapercave.com/wp/wp2306605.jpg'
+  },
+  {
+    id: 7,
+    location: 'Italy',
+    date: '2021-04-01',
+    price: '75',
+    image: 'https://cdn.statically.io/img/lh4.googleusercontent.com/proxy/_OYtNrXCdUjG1b6gzuIOBwfzfKNbv8CdR1JtxX5H80V3ZYgifjM5MFTnZWlM3dOPAJ2ryYfr75evZ_8Br53ZatJ0wDWvOiG2saMYOUqlZs6ZxFQEw9mAQHKiGhOZFQaw=s0-d'
+  },
+  {
+    id: 8,
+    location: 'Puerto Rico',
+    date: '2021-08-01',
+    price: '100',
+    image: 'https://wallpaperaccess.com/full/843169.jpg'
+  },
+  {
+    id: 9,
+    location: 'London',
+    date: '2021-06-20',
+    price: '85',
+    image: 'https://wallpaperaccess.com/full/172520.jpg'
+  },
+  {
+    id: 10,
+    location: 'Paris',
+    date: '2021-05-15',
+    price: '115',
+    image: 'https://wallpaperaccess.com/full/296466.jpg'
   }
 ]
 
